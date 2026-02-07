5a78a7813c817b9f25f2ee824680b87df4ec52e1
Ethan Young <ethan.young@cedar.com>
2026-02-06 17:43:17 -0700

    Add Pool option for Postgres backend

--- backend/postgres/README.md
+++ backend/postgres/README.md
@@ -1,5 +1,1 @@
 # Postgres
-
-Postgres event store is still under development and is missing the following features:
-
-- Aggregate version consistency validation
--- backend/postgres/store_test.go
+++ backend/postgres/store_test.go
@@ -3,10 +3,14 @@
 package postgres_test
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"sync/atomic"
 	"testing"
+	"time"
 
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/modernice/goes/backend/postgres"
 	"github.com/modernice/goes/backend/testing/eventstoretest"
 	"github.com/modernice/goes/codec"
@@ -15,7 +19,35 @@ import (
 
 func TestEventStore(t *testing.T) {
 	eventstoretest.Run(t, "postgres", func(enc codec.Encoding) event.Store {
-		return postgres.NewEventStore(enc, postgres.Database(nextDatabase()))
+		time.Sleep(60 * time.Millisecond) // Wait for previous connections to close
+		return postgres.NewEventStore(enc, postgres.URL(fmt.Sprintf("%s?pool_max_conn_lifetime=50ms", os.Getenv("POSTGRES_EVENTSTORE"))), postgres.Database(nextDatabase()))
+	})
+}
+
+func TestEventStoreWithPool(t *testing.T) {
+	eventstoretest.Run(t, "postgres", func(enc codec.Encoding) event.Store {
+		ctx := context.Background()
+
+		pool, err := pgxpool.Connect(ctx, os.Getenv("POSTGRES_EVENTSTORE"))
+		if err != nil {
+			t.Fatalf("Failed to create bootstrapping connection to server: %v", err)
+		}
+
+		database := nextDatabase()
+		_, err = pool.Exec(ctx, fmt.Sprintf("CREATE DATABASE %s", database))
+		if err != nil {
+			pool.Close()
+			t.Fatalf("create %q database: %v", database, err)
+		}
+		pool.Close()
+
+		time.Sleep(60 * time.Millisecond) // Wait for previous connections to close
+		pool, err = pgxpool.Connect(ctx, fmt.Sprintf("%s/%s?pool_max_conn_lifetime=50ms", os.Getenv("POSTGRES_EVENTSTORE"), database))
+		if err != nil {
+			t.Fatalf("Failed to create connection to database %q: %v", database, err)
+		}
+
+		return postgres.NewEventStore(enc, postgres.Pool(pool))
 	})
 }
 
